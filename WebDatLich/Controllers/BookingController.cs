1ad0a6d4704def5d2a2a404e1e39d7738f90697b
Ayashion <phuc9213@gmail.com>
2024-12-02 00:55:30 +0700

    Add Booking page and CheckOut Page
    Thêm View danh sách các List mà tài khoản khách hàng đã book
    
    Thêm View và controller của trang checkout
    
    Sủa lại hiển thị tên người dùng ở Góc trên thay vì hiển thị là "Your Booking"

---
+++ WebDatLich/Controllers/BookingController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebDatLich.Data;
+
+
+namespace WebDatLich.Controllers
+{
+    public class BookingController : Controller
+    {
+        private readonly CsdlDuLichContext _context;
+
+        public BookingController(CsdlDuLichContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var userid = HttpContext.Session.GetString("Username");
+            var bookings = _context.Bookings
+                .Include(b => b.Tour)
+                    .ThenInclude(c => c.Destination)
+                .Include(d => d.Customer)
+                    .ThenInclude(e => e.Accounts)
+                .Where(b => b.Customer.Accounts.Any(a => a.Username == userid))
+                .ToList();
+            return View(bookings);
+        }
+    }
+}
